a2b697015ec3bc5e979a2e2d946791a4d934e338
Marcelo Rodrigues Alves <marceloalves_ac@hotmail.com>
2022-05-03 15:51:29 +0100

    changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('codificando....');
+  res.end('novo sistema de commits e deploy automatizado by Marcelo Alves');
 });
 
 server.listen(port, hostname, () => {
