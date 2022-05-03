5f9bff56bdc1d47383678bd4782b37e3ad49919c
Marcelo Rodrigues Alves <marceloalves_ac@hotmail.com>
2022-05-03 10:30:53 +0100

    second deploy test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('First deploy automatic test');
+  res.end('second deploy automatic test');
 });
 
 server.listen(port, hostname, () => {
