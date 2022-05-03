ec3975caed2cde795c2fd3849112840e5dd5b7f5
Marcelo Rodrigues Alves <marceloalves_ac@hotmail.com>
2022-05-03 12:11:42 +0100

    deploy auto

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('testes 2222');
+  res.end('deploy automatico github');
 });
 
 server.listen(port, hostname, () => {
