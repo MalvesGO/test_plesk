c3d9aca11efa0e0890572b354fa99949f2e7dde4
Marcelo Rodrigues Alves <marceloalves_ac@hotmail.com>
2022-05-03 14:35:06 +0100

    key

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const hostname = '127.0.0.1';
 const port = 5000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.json({
-    message: 'Hello World',
-    status: true
-  })
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+        key: "value"
+    }));
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
