38a765188787b69f44b494ab0e85860c8e88afa6
Marcelo Rodrigues Alves <marceloalves_ac@hotmail.com>
2022-05-03 16:40:48 +0100

    dev commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('novo sistema de commits e deploy automatizado by Tiago Oliveira');
+  res.end('Novo Commit a partir do dev');
 });
 
 server.listen(port, hostname, () => {
